0f8ef46ff78807af111cd0e4ae49722a4ed14cb6
Marc Schiller <m4rc.schiller@gmail.com>
2023-04-08 17:00:49 +0200

    Finish Bikeleasing

--- pages/bikeleasing.tsx
+++ pages/bikeleasing.tsx
@@ -1,6 +1,7 @@
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import Head from "next/head";
+import Image from "next/image";
 
 export default function Bikeleasing() {
   return (
@@ -16,7 +17,31 @@ export default function Bikeleasing() {
           </div>
         </header>
         <main>
-          <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">{/* Your content */}</div>
+          <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+            <p className="pt-2 pb-4 px-3">
+              <Image width={800} height={167} src="/bikeleasing.png" className="mx-auto" alt="" />
+            </p>
+            <p className="pt-2 pb-4 px-3 text-justify">
+              Dienstradeln erlebt Boom wie nie Dienstfahrräder als Arbeitgeberleistung – an diesem Trend kommt im
+              Moment kaum ein Unternehmen mehr vorbei, das mit der Zeit gehen möchte.
+            </p>
+            <h3 className="font-bold text-xl text-teal-900 px-4">Einfaches Handling, Vorteile für alle</h3>
+            <p className="pt-2 pb-4 px-3 text-justify">
+              Dienstrad-Leasing ist so einfach wie wirkungsvoll: Leasingnehmer ist der Arbeitgeber, die Leasingrate
+              trägt der Arbeitnehmer durch den Einbehalt eines Teils des monatlichen Bruttogehalts in Höhe der
+              Leasingrate. Die Mitarbeiter kommen zu attraktiven Raten und mit einer Einsparung von bis zu 40 Prozent
+              gegenüber Kauf zu ihrem Traumfahrrad, das sie auch privat nutzen können. Die Auswahl trifft dabei jeder
+              Mitarbeiter persönlich beim regionalen Fahrradhändler. Schon ab nur einem Dienstrad ist Leasing möglich,
+              bei einer Preisspanne von 649,00 bis 15.000,00 Euro pro Wunsch-Bike. Die Bikeleasing-Service GmbH ist
+              dafür zentraler Ansprechpartner und positioniert sich mit über 10.000 Kooperationspartnern und einem
+              Full-Service inklusive Versicherung als erfahrener Anbieter in diesem Segment deutschlandweit. Mehr
+              Informationen sowie einen Vorteilsrechner unter{" "}
+              <a className="text-teal-900 hover:text-teal-500" target="_blank" href="http://www.bikeleasing.de">
+                www.bikeleasing.de
+              </a>
+              .
+            </p>
+          </div>
         </main>
         <Footer />
       </div>
